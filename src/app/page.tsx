8814da7896169170b55ff7ccfc6ece31edb2e471
Seth Burns <seth@sethburns.com>
2024-04-15 11:34:45 -0700

    Catch up commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,147 @@
-import Image from "next/image";
+'use client';
+import { ChangeEvent, SetStateAction, useEffect, useState } from 'react';
+
+import Papa from 'papaparse';
+
+interface ObjectWithPrice {
+  'Part #': string;
+  'Primary Description': string;
+  'Auxiliary Description': string;
+  'Vendor Part Number': string;
+  Barcode: string;
+  'Vendor Name': string;
+  Category: string;
+  Manufacturer: string;
+  Size: string;
+  'Color I': string;
+  'Color II': string;
+  'Accent Color': string;
+  'Model Number': string;
+  Cost: string;
+  MSRP: string;
+  Retail: string;
+  'Gross Margin %': string;
+  'Minimum Price': string;
+  'Sale Price': string;
+  'Package Price': string;
+  'Preferred Customer Price': string;
+  Staff: string;
+  Government: string;
+  Wholesale: string;
+  'Online Price': string;
+  'Apply Tax 1 (Yes/No)': string;
+  'Apply Tax 2 (Yes/No)': string;
+  'Apply Tax 3 (Yes/No)': string;
+  'No Commission flag': string;
+  'Commission Bonus': string;
+  'On Sale': string;
+  'Ignore Roundup': string;
+  'Discount Value (%)': string;
+  'Quantity on Hand': string;
+  'Make Miscelleneous Item': string;
+  "Don't check for inventory in Stock Flag": string;
+  'Reorder Level': string;
+  'Preferred Stock Level': string;
+  'Reorder Quantity': string;
+  'Unit of Measurement': string;
+  'Case Quantity': string;
+  'Stocking Location': string;
+  'Print Label for each item in Stock': string;
+  'Warranty Reminder': string;
+  'Popup Notes': string;
+  'POS Visibility': string;
+  'Last Received Date': string;
+  'Website Visibility': string;
+  'Online Title': string;
+  'Online Description': string;
+  'Free Shipping': string;
+  'Display in Product Listing': string;
+  'Min. Advertised Pricing Flag': string;
+  'Min. Advertised Pricing': string;
+  'Image 1': string;
+  'Image 2': string;
+  'Image 3': string;
+  'Image 4': string;
+  'Image 5': string;
+  'Product Url': string;
+}
 
 export default function Home() {
+  const [data, setData] = useState<any[]>([]);
+  const [data2, setData2] = useState<any[]>([]);
+  const [finalCsv, setFinalCsv] = useState<string>('');
+
+  const [error, setError] = useState<string>('');
+
+  const handleFile = (event: ChangeEvent<HTMLInputElement>) => {
+    Papa.parse(event.target.files?.[0]!, {
+      header: true,
+      error: function (err) {
+        console.log(err);
+      },
+      complete: function (results) {
+        setData(results.data);
+        console.log(results ?? 'no results');
+      },
+    });
+  };
+
+  const handleFileTwo = (event: ChangeEvent<HTMLInputElement>) => {
+    Papa.parse(event.target.files?.[0]!, {
+      header: true,
+      complete: function (results2) {
+        setData2(results2.data);
+      },
+    });
+  };
+
+  const combineDuplicates = (mergedArray: ObjectWithPrice[]) => {
+    const result: ObjectWithPrice[] = [];
+    const map: { [key: string]: ObjectWithPrice } = {};
+
+    mergedArray.forEach((item) => {
+      const key = item.Barcode;
+
+      if (map[key]) {
+        map[key][`Quantity on Hand`] = (
+          parseInt(map[key][`Quantity on Hand`], 10) +
+          parseInt(item[`Quantity on Hand`], 10)
+        ).toString();
+      } else {
+        map[key] = { ...item };
+      }
+    });
+
+    for (const key in map) {
+      result.push(map[key]);
+    }
+    console.log(result);
+    setFinalCsv(Papa.unparse(result, { delimiter: '\n' }));
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="">
+      <div className="">
+        <h1>DS360 to Amazon</h1>
+        <input
+          type="file"
+          name="file"
+          accept=".csv"
+          onChange={(e) => handleFile(e)}
+          className="m-2"
+        ></input>
+        <input
+          type="file"
+          name="file"
+          accept=".csv"
+          onChange={(e) => handleFileTwo(e)}
+          className="m-2"
+        ></input>
+        <button onClick={() => combineDuplicates([...data, ...data2])}>
+          Combine
+        </button>
+        <br />
+        <div>{finalCsv}</div>
       </div>
     </main>
   );
